25105979e1da4f431c3c09cf5368ac54e6290866
Agilio Padua <agilio.padua@gmail.com>
2021-11-15 15:45:39 +0100

    Script update

--- omm.py
+++ omm.py
@@ -10,6 +10,7 @@ from simtk.openmm import app
 
 field = 'field-p-sc.xml'
 config = 'config-p.pdb'
+#config = 'last.pdb'
 
 temperature = 353.0*unit.kelvin
 pressure = 1.0*unit.bar
@@ -30,8 +31,8 @@ print('#  ', modeller.topology.getNumResidues(), 'molecules',
     modeller.topology.getNumBonds(), 'bonds')
 
 lx = modeller.topology.getUnitCellDimensions().x
-ly = modeller.topology.getUnitCellDimensions().x
-lz = modeller.topology.getUnitCellDimensions().x
+ly = modeller.topology.getUnitCellDimensions().y
+lz = modeller.topology.getUnitCellDimensions().z
 print('#   box', lx, ly, lz, 'nm')
 
 system = forcefield.createSystem(modeller.topology, nonbondedMethod=app.PME,
@@ -53,28 +54,35 @@ system.addForce(barostat)
 
 platform = openmm.Platform.getPlatformByName('CUDA')
 #platform.setPropertyDefaultValue('Precision', 'mixed')
-properties = {'Precision': 'mixed'}
+properties = {'DeviceIndex': '1', 'Precision': 'mixed'}
 
 sim = app.Simulation(modeller.topology, system, integrator, platform, properties)
 sim.context.setPositions(modeller.positions)
 sim.context.setVelocitiesToTemperature(temperature)
 
 ## read coords and velocities from checkpoint (replaces sim.context.setPositions and setVelocitiesToTemperature)
-## only the same machine
-# print('# coordinates and velocities from equil.chk')
-# sim.loadCheckpoint('equil.chk')
+## do only on the same machine
+#print('# coordinates and velocities from equil.chk')
+#sim.loadCheckpoint('equil.chk')
 
 ## read coords and velocities from restart file (replaces sim.context.setPositions and setVelocitiesToTemperature)
 ## any machine
-# print('# coordinates and velocities from equil.rst')
-# with open('equil.rst', 'r') as f:
-#    sim.context.setState(openmm.XmlSerializer.deserialize(f.read()))
+#print('# coordinates and velocities from equil.rst')
+#with open('equil.rst', 'r') as f:
+#   sim.context.setState(openmm.XmlSerializer.deserialize(f.read()))
+
+#print('# coordinates and velocities from state.xml')
+#sim.loadState('state.xml')
+
+#state = sim.context.getState()
+#sim.topology.setPeriodicBoxVectors(state.getPeriodicBoxVectors())
 
 platform = sim.context.getPlatform()
 print('# platform', platform.getName())
 for prop in platform.getPropertyNames():
     print('#  ', prop, platform.getPropertyValue(sim.context, prop))
 
+# minimize only when starting from fresh conf, not after equilibration
 print('# minimizing...')
 sim.minimizeEnergy(maxIterations=1000)
 state = sim.context.getState(getEnergy=True,getForces=True,getVelocities=True,getPositions=True)
@@ -86,9 +94,9 @@ sim.reporters = []
 sim.reporters.append(app.StateDataReporter(sys.stdout, 1000, step=True,
     speed=True, temperature=True, separator='\t',
     totalEnergy=True, potentialEnergy=True, density=True))
-sim.reporters.append(app.PDBReporter('traj.pdb', 10000))
-sim.reporters.append(app.DCDReporter('traj.dcd', 10000))
-sim.reporters.append(app.CheckpointReporter('equil.chk', 200000))
+sim.reporters.append(app.PDBReporter('traj.pdb', 5000))
+#sim.reporters.append(app.DCDReporter('traj.dcd', 5000))
+sim.reporters.append(app.CheckpointReporter('equil.chk', 100000))
 
 kB = unit.BOLTZMANN_CONSTANT_kB/(unit.joule/unit.kelvin)
 NA = unit.AVOGADRO_CONSTANT_NA*unit.mole
@@ -143,9 +151,11 @@ coords = state.getPositions()
 sim.topology.setPeriodicBoxVectors(state.getPeriodicBoxVectors())
 app.PDBFile.writeFile(sim.topology, coords, open('last.pdb', 'w'))
 
+#sim.saveState('state.xml')
+
 # write restart file at the end of the run
-with open('equil.rst', 'w') as f:
-    f.write(openmm.XmlSerializer.serialize(state))
+#with open('equil.rst', 'w') as f:
+#    f.write(openmm.XmlSerializer.serialize(state))
 
 print()
 print('#', datetime.datetime.now())
